33bee5b50b7fdf6915935e0062583e1b7a94bcdb
Balasaheb02 <thakarebalasaheb02@gmail.com>
2021-12-28 07:56:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-9vc6xb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-9vc6xb)
---
+++ src/app/reverse-string.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'reverseString'
+})
+export class ReverseStringPipe implements PipeTransform {
+  transform(value: string): string {
+    let newStr: string = "";
+    for (let i = value.length - 1; i >= 0; i-- ) {
+    newStr += value.charAt(i);
+  }
+    return newStr;
+  }
+
+}
